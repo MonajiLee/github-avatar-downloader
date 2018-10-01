8615114ce2c6854aa210fea4c586663c19cfdd29
MonajiLee <lee.smonica@gmail.com>
2018-10-01 23:13:59 +0000

    Started the getRepoContributors function without callback

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,21 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// request('URL', function(err, response, body){
+//     if (err){
+//         console.log('WRONG', err);
+//         return false
+//     }
+// })
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    request.get('https://api.github.com/repos/jquery/jquery/contributors')
+    console.log(repoOwner);
+    console.log(repoName);
+}
+
+// TEST VALUES
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
