cf3e505256f72dea94751f35f6b66cc69d7e3bd4
MonajiLee <lee.smonica@gmail.com>
2018-10-02 03:11:36 +0000

    Combined functions getRepoContributors and downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -18,47 +18,45 @@ var fs = require('fs');
     //     })
 
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//     var options = {
-//         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//         headers: {
-//             'User-Agent': 'request',
-//             'Authorization': "token " + secret.GITHUB_TOKEN
-//         }
-//     }
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': "token " + secret.GITHUB_TOKEN
+        }
+    }
 
-//     request(options, function(err, res, body) {
-//         if (err){
-//             console.log('Errors:', err)
-//         } else {
-//             var json = JSON.parse(body);        // array of objects
-//             cb(null, findURLs(json));
-            
-//         }
-//     })
+    request(options, function(err, res, body) {
+        if (err){
+            console.log('Errors:', err)
+        } else {
+            var json = JSON.parse(body);
+            cb(null, findURLs(json));           // why do we use cb vs downloadImageByURL?
+        }
+    })
 
-//     function findURLs(json) {
-//         var newArray = []
-//         json.forEach(function(element){
-//         newArray.push(element.avatar_url);
-//         })
-//         return newArray;
-//     }
-// }
+    function findURLs(json) {
+        json.forEach(function(element){
+            downloadImageByURL(element.avatar_url, 'avatars/' + element.login + '.jpg');
+        })
+    }
+}
+
+// cb loops through each item in the array:
+// It constructs a file path using the login value (e.g., "avatars/dhh.jpg")
+// It then passes the avatar_url value and the file path to downloadImageByURL
 
 function downloadImageByURL(url, filePath) {
-    // url = 'https://avatars2.githubusercontent.com/u/2741?v=3&s=466'
-    // filePath = 'avatars/kvirani.jpg';
     let downloadURL = request.get(url).pipe(fs.createWriteStream(filePath));
     return downloadURL;
 }
 
 
+// downloadImageByURL("https://avatars3.githubusercontent.com/u/1199584?v=4", "avatars/rosy.jpg")
 
-downloadImageByURL("https://avatars3.githubusercontent.com/u/1199584?v=4", "avatars/rosy.jpg")
 
-// TEST VALUES
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//     console.log("Errors:", err);
-//     console.log("Result:", result);
-// });
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
