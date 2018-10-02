0048ccea80713a910633c765ea7dd396934687e2
MonajiLee <lee.smonica@gmail.com>
2018-10-02 01:24:42 +0000

    getRepoContributors function prints avatar_urls for each contributor

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var secret = require('./secret');
 
 
 // request('URL', function(err, response, body){
@@ -8,28 +9,60 @@ var request = require('request');
 //     }
 // })
 
+      // getRepoContributors(url, function(err, res, body)
+// function getRepoContributors(repoOwner, repoName, cb) {
+    // request.get('https://api.github.com/repos/jquery/jquery/contributors')
+    //     .on('error', function(err){
+    //         console.log('Failure');
+    //     })
+
+    //     .on('response', function(response){
+    //         console.log(repoOwner);
+    //         console.log(repoName);
+    //     })
+
+    //     .on('end', function(){
+    //         console.log('Run operation completed.')
+    //     })
+
+// var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    request.get('https://api.github.com/repos/jquery/jquery/contributors')
-    
-    var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-    request(url, function(err, res, body) {
-      cb(err, body);
-    })
-    
-    .on('error', function(err){
-        console.log('Failure');
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': "token " + secret.GITHUB_TOKEN
+        }
+    }
+
+// Request is designed to be the simplest way possible to make HTTP calls.
+    request(options, function(err, res, body) {
+        if (err){
+            console.log('Errors:', err)
+        } else {
+            var json = JSON.parse(body);        // array of objects
+            cb(null, findURLs(json));
+            
+        }
     })
 
-    .on('response', function(response){
-        console.log(repoOwner);
-        console.log(repoName);
-    })
-
-    .on('end', function(){
-        console.log('Run operation completed.')
-    })
+    function findURLs(json) {
+        var newArray = []
+        json.forEach(function(element){
+        newArray.push(element.avatar_url);
+        })
+        return newArray;
+    }
 }
+
+
+
+    // callback
+//     request(url, function(err, res, body) {
+//         cb(err, body);
+//     })
+// }
 
 
 // TEST VALUES
